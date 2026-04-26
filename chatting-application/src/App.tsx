b6828638e5afb9bacf928937df9307ec68255bb8
Rawat890 <rohanrawat7508@gmail.com>
2026-04-26 09:09:46 +0530

    chore: logic for maintaining user session applied using zustand

--- chatting-application/src/App.tsx
+++ chatting-application/src/App.tsx
@@ -2,13 +2,47 @@ import List from './components/list/List'
 import Chat from './components/chat/Chat'
 import Details from './components/details/Details'
 import './App.css';
+import Login from './components/login/Login';
+import Notification from './components/notification/Notification';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../src/lib/firebase';
+import { useUserStore } from './lib/userStore';
 
 function App() {
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo])
+
+  console.log(currentUser)
+
+  if (isLoading) {
+    return <div className="loader">
+      Loading....
+    </div>
+  }
+
   return (
     <div className="container">
-      <List />
-      <Chat />
-      <Details />
+      {currentUser ? (
+        <>
+          <List />
+          <Chat />
+          <Details />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
+      <Notification />
     </div>
   )
 }
