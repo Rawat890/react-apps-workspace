a9165e1c359a311c2f2e5040750affba11d22200
Rawat890 <rohanrawat7508@gmail.com>
2026-04-26 16:19:22 +0530

    feat: onSnapshot method used to get chats and show the list of users

--- chatting-application/src/components/list/chatList/ChatList.tsx
+++ chatting-application/src/components/list/chatList/ChatList.tsx
@@ -1,9 +1,42 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './chatList.css';
+import AddUser from '../../addUser/AddUser';
+import { useUserStore } from '../../../lib/userStore';
+import { useChatStore } from '../../../lib/chatStore';
+import { db } from '../../../lib/firebase';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
 
 function ChatList() {
+  const [chats, setChats] = useState([]) as any;
   const [addMode, setAddMode] = useState(false);
+  const { currentUser } = useUserStore();
+  const { changeChat } = useChatStore();
 
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "userChats", currentUser.id), async (res) => {
+      const items = res.data().chats;
+      const promises = items.map(async (item) => {
+        const userDocRef = doc(db, "users", item.receiverId);
+        const userDocSnap = await getDoc(userDocRef);
+        const user = userDocSnap.data();
+        return { ...item, user };
+      })
+
+      const chatData = await Promise.all(promises);
+      setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt))
+
+    })
+
+    return () => {
+      unsub();
+    }
+  }, [currentUser.id])
+
+  console.log("Chats - ", chats)
+
+  const handleSelect = async (chat) => {
+    changeChat(chat.chatId, chat.user)
+  }
   return (
     <div className='chatList'>
       <div className="search">
@@ -16,70 +49,16 @@ function ChatList() {
         />
       </div>
 
-      <div className="item">
-        <img src='../../../../public/images/avatar.png' alt='' />
-        <div className="texts">
-          <span>John</span>
-          <p> hello</p>
+      {chats.map((chat) => (
+        <div className="item" key={chat.chatId} onClick={() => handleSelect(chat)}>
+          <img src={chat.user.avatar || '../../../../public/images/avatar.png'} alt='' />
+          <div className="texts">
+            <span>{chat.user.username}</span>
+            <p>{chat.lastMessage}</p>
+          </div>
         </div>
-      </div>
-
-      <div className="item">
-        <img src='../../../../public/images/avatar.png' alt='' />
-        <div className="texts">
-          <span>John</span>
-          <p> hello</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src='../../../../public/images/avatar.png' alt='' />
-        <div className="texts">
-          <span>John</span>
-          <p> hello</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src='../../../../public/images/avatar.png' alt='' />
-        <div className="texts">
-          <span>John</span>
-          <p> hello</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src='../../../../public/images/avatar.png' alt='' />
-        <div className="texts">
-          <span>John</span>
-          <p> hello</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src='../../../../public/images/avatar.png' alt='' />
-        <div className="texts">
-          <span>John</span>
-          <p> hello</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src='../../../../public/images/avatar.png' alt='' />
-        <div className="texts">
-          <span>John</span>
-          <p> hello</p>
-        </div>
-      </div>
-
-      <div className="item">
-        <img src='../../../../public/images/avatar.png' alt='' />
-        <div className="texts">
-          <span>John</span>
-          <p> hello</p>
-        </div>
-      </div>
-
+      ))}
+      {addMode ? <AddUser /> : null}
     </div>
   )
 }
